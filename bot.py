b0ed22b7901d4dafc1fcd292508e04215100d840
beastfactx <beastfactx@gmail.com>
2026-02-04 00:49:19 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,41 @@
+import os
+from telegram import Update
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    MessageHandler,
+    ContextTypes,
+    filters,
+)
+
+BOT_TOKEN =os.getenv("5850270544:AAFEhxWNzlaTgFX_MJE3hWkrxXlO5aKhkRc")  # token comes from Render
+
+VIDEO_ID = "BAACAgUAAxkBAAMyaYJEtL-97XDB9iMJPlBzczmiZQkAAkwcAAJrfhlUcSJQJJiqAR44BA"
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "എന്തൂട്ര ഗെടിയെ🐘🎆🪅"
+    )
+
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.message.text and update.message.text.lower() == "pappy":
+        await update.message.reply_video(
+            video=VIDEO_ID,
+            supports_streaming=True
+        )
+
+
+def main():
+    app = Application.builder().token(BOT_TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+
+    print("പെടന്നെ പെട🌈")
+    app.run_polling()
+
+
+if __name__ == "__main__":
+    main()
